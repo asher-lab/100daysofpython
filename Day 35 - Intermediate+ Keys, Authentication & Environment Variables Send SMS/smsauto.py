c9c29ef51986e88ba325e91835169f28f06a6e2b
Asher <51806917+asher-lab@users.noreply.github.com>
2021-08-05 20:07:22 +0800

    Update smsauto.py

--- Day 35 - Intermediate+ Keys, Authentication & Environment Variables Send SMS/smsauto.py
+++ Day 35 - Intermediate+ Keys, Authentication & Environment Variables Send SMS/smsauto.py
@@ -4,7 +4,8 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = "ACe86c64401253b9401e5eed67594c5576"
-auth_token = "12074907b044ff5a9a1d83998aaf8abe"
+#auth_token = "12074907b044ff5a9a1d83998aaf8abe"
+auth_token =  os.environ.get("TWILIO_KEY")
 client = Client(account_sid, auth_token)
 print(client)
 
